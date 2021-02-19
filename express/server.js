e0a79e104bc6123e669861cc714d83369df8b52c
AtomOpera <64531968+AtomOpera@users.noreply.github.com>
2021-02-19 14:37:39 +0000

    adding my code

--- express/server.js
+++ express/server.js
@@ -4,15 +4,53 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const fs = require("fs");
+
+function saveFile(input) {
+  
+  fs.writeFile("./express/test.json", input, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("The file was saved!");
+    }
+  });
+  return 0;
+};
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+  fs.readFile("./express/test.json", 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    //res.send(JSON.parse(data));
+    //dataInTxt = JSON.stringify(data);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Hello from Express.js!</h1>');
+    res.write(JSON.stringify(JSON.parse(data)));
+    res.end();
+  });
+  
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+// Create a new entry
+router.post('/new', (req, res) => {
+  console.log(req.body);      // your JSON
+  saveFile(JSON.stringify(req.body));
+  res.send(req.body);    // echo the result back
+});
+
+router.get('/getData', (req, res) => {
+  fs.readFile("./src/test.json", 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    res.send(JSON.parse(data));
+    //dataInTxt = JSON.stringify(data);
+  });
+  // res.send(dataInTxt);
+});
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
